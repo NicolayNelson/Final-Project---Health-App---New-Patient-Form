096d3caf394abe27d6cc02739fbf804afa5e8a6d
NicolayNelson <59945806+NicolayNelson@users.noreply.github.com>
2020-05-15 19:38:23 -0700

    upload of draft

--- src/edu/whccd/nnelson/Main.java
+++ src/edu/whccd/nnelson/Main.java
@@ -24,48 +24,64 @@ public class Main {
         // Enter year of birth for 4 digits
 
 
-        // Limits the number to 1000-Current Year
-        boolean isValidAge = true;
+        // Limits the number to 1000-Current
+
+        boolean isValidAge = false;
+        int yearOfBirth;
         do {
             System.out.println("Year of birth 4 digits (1000 - Current Year):\t");
-            int yearOfBirth = input.nextInt();
-            if (yearOfBirth > 1000 || yearOfBirth <= LocalDateTime.now().getYear()) {
+            yearOfBirth = input.nextInt();
+            if (yearOfBirth > 1000 && yearOfBirth <= LocalDateTime.now().getYear()) {
                 isValidAge = true;
             }
-        }while (!isValidAge); 
+        }while (!isValidAge);
+
             int age = LocalDateTime.now().getYear() - yearOfBirth;
 
-            // Set ups a gender question and input
-            System.out.print("Gender (M => Male, F => Female, O => Other / Not Listed):\t");
-            String gender = input2.nextLine();
 
-            // Sets up a variable for the console log for what the user inputs
-            if (gender.equalsIgnoreCase("m")) {
-                gender = "Male";
-            } else if (gender.equalsIgnoreCase("male")) {
-                gender = "Male";
-            } else if (gender.equalsIgnoreCase("f")) {
-                gender = "Female";
-            } else if (gender.equalsIgnoreCase("female")) {
-                gender = "Female";
-            } else {
-                gender = "Other / Not Listed (" + gender + ")";
-            }
+            // Set ups a gender question and input
+            boolean gender1 = false;
+            String gender;
+            do {
+                System.out.print("Gender (M => Male, F => Female, O => Other / Not Listed):\t");
+                gender = input2.nextLine();
+                switch ( gender ) {
+                    case "m":
+                    case "M":
+                        gender = "Male";
+                        gender1 = true;
+                        break;
+                    case "f":
+                    case "F":
+                        gender = "Female";
+                        gender1 = true;
+                        break;
+                    case "o":
+                    case "O":
+                        gender = "Other / Not Listed";
+                        gender1 = true;
+                        break;
+                    default:
+                        System.out.println("Please enter either M/F/O");
+                        break;
+                }
+            } while (!gender1);
+
 
             // Questions to be answered.
 
             ArrayList<String> questions = new ArrayList<>();
-            questions.add("[1]Has the patient traveled to anyplace outside of the United States recently:\t");
-            questions.add("[2]Does the patient have any known conditions:\t");
-            questions.add("[3]Is the patient allergic to any type of medicine:\t");
-            questions.add("[4]Was the patient admitted before:\t");
-            questions.add("[5]Family medical history:\t");
-            questions.add("[6]Current medications the patient is on:\t");
-            questions.add("[7]Recent surgeries:\t");
-            questions.add("[8]How much alcohol does the patient consume a week:\t");
-            questions.add("[9]Does the patient have a smoking history:\t");
-            questions.add("[10]Does the patient have insurance:\t");
-            questions.add("[11]Emergency contact information:\t");
+            questions.add("Has the patient traveled to anyplace outside of the United States recently:\t");
+            questions.add("Does the patient have any known conditions:\t");
+            questions.add("Is the patient allergic to any type of medicine:\t");
+            questions.add("Was the patient admitted before:\t");
+            questions.add("Family medical history:\t");
+            questions.add("Current medications the patient is on:\t");
+            questions.add("Recent surgeries:\t");
+            questions.add("How much alcohol does the patient consume a week:\t");
+            questions.add("Does the patient have a smoking history:\t");
+            questions.add("Does the patient have insurance:\t");
+            questions.add("Emergency contact information:\t");
 
             // ArrayList for the answers
             ArrayList<String> answers = new ArrayList<>();
@@ -87,9 +103,11 @@ public class Main {
             System.out.println("User's age: " + age);
             System.out.println("User's Gender:  " + gender);
 
+            int counter = 1;
             for (int i = 0; i < questionsAndAnswers.size(); i++) {
                 if (i % 2 == 0) {
-                    System.out.println((i + 1) + ". " + questionsAndAnswers.get(i));
+                    System.out.println((counter) + ". " + questionsAndAnswers.get(i));
+                    counter++;
                 } else {
                     System.out.println("\t- " + questionsAndAnswers.get(i));
                 }
